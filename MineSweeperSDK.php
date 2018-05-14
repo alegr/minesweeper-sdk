2822982219ade4d2351c325689718e22f3429005
Alejandro Garcia del Rio <alejandro.garciadelrio@gmail.com>
2018-05-14 08:54:43 -0300

    Create MineSweeperSDK class

---
+++ MineSweeperSDK.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * MineSweeper wrapper for API communication
+ *
+ * @package MineSweeperSDK
+ * @author Alejandro Garcia del Rio <alejandro.garciadelrio@gmail.com>
+ */
+class MineSweeperSDK 
+{
+
+  /**
+   * @version 1.0.0
+   */
+  const VERSION  = "1.0.0";
+
+}
